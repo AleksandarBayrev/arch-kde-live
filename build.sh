f828ddd92905974419dbfd4f7e957bd066848d57
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2022-07-25 20:06:32 +0300

    Add clean of out on starting of build

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Cleaning ./work"
 sudo rm -rfv ./work
+echo "Cleaning ./out"
+sudo rm -rfv ./out
 echo "Starting build process of Arch Linux KDE"
 sudo mkarchiso -v .
 sudo rm -rfv ./work
